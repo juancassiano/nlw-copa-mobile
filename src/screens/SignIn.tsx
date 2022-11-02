6b46b9ca64dd36bfd79c27bdb68cb66cce1710df
Juan Cassiano <juancassiano@hotmail.com>
2022-11-02 19:10:19 -0300

    Criação do componente Button e interface de SignIn

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,10 +1,23 @@
-import { Text, Center } from "native-base";
+import { Text, Center, Icon } from "native-base";
+import Logo from '../assets/logo.svg';
+import { Button } from "../components/Button";
+import { Fontisto } from '@expo/vector-icons';
+
 
 export function SignIn(){
   return(
-    <Center flex={1} bgColor="gray.900">
-      <Text color="white" fontSize={24} fontFamily="heading">
-        SignIn
+    <Center flex={1} bgColor="gray.900" p={7}>
+      <Logo width={212} height={40}/>
+
+      <Button 
+        type="SECONDARY"
+        title="ENTRAR COM GOOGLE"
+        leftIcon={<Icon as={Fontisto} name="google" color="white" size="md" />}
+        mt={12}
+      />
+      
+      <Text color="white" textAlign="center" mt={4}>
+        Não utilizamos nenhuma informação além {'\n'} de seu email para a criação da sua conta.
       </Text>
     </Center>
   )
